ef1ea6fd09d87b21b37371b7172d63a314f96475
Andy Herrmann <andrewjherrmann@gmail.com>
2023-03-02 13:02:38 -0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar icon="date_range" size="xl">
+          </q-avatar>
+          PTO Projection Tool
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
